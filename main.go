21beef4ddeca73be8dbf485da1f9a9760d1cf8b0
Oliver Trabhardt <oliver.trabhardt@googlemail.com>
2022-06-24 20:11:08 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 )
+
+const MAX_CONCURRENT_JOBS = 30
 
 func urlFuzzScanner(url string, directoryList []string, showStatus string) {
 	// open the text file directoryList and read the lines in it
@@ -19,38 +21,47 @@ func urlFuzzScanner(url string, directoryList []string, showStatus string) {
 	defer file.Close()
 	// read the lines in the text file
 	scanner := bufio.NewScanner(file)
-
+	waitChan := make(chan struct{}, MAX_CONCURRENT_JOBS)
+	count := 0
 	for scanner.Scan() {
-		// get the line in the text file
 		line := scanner.Text()
 		// check if the line is empty
 		if line == "" {
 			continue
 		}
-		// test the lin a go rountine
-		go testUrl(url, line, showStatus)
+		// get the line in the text file
+
+		waitChan <- struct{}{}
+		count++
+		go func(count int, url string, line string, showStatus string) {
+			testUrl(url, line, showStatus)
+			job(count)
+			<-waitChan
+		}(count, url, line, showStatus)
 	}
 }
 
 func testUrl(url string, line string, showStatus string) {
 	// create a new http client
-	client := &http.Client{}
+	// client := &http.Client{}
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
 	// create a new request
 	url = url + "/" + line
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
-	
 	// set the user agent
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36")
-	
-	// make the request
+	// make the request w
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
 	}
-	
 	// check the response status code
 	if resp.StatusCode == 200 {
 		// if the response status code is 200, print the url
@@ -66,14 +77,15 @@ func main() {
 	url := os.Args[1]
 	// get directoryList parameter from name "directoryList" in the command line
 	directoryList := strings.Split(os.Args[2], ",")
-	start := time.Now()
 	// get status parameter from the command lline
 	status := os.Args[3]
 
 	// check the directory list, if the found in the url
 	urlFuzzScanner(url, directoryList, status)
+}
 
-	time.Sleep(30 * time.Second)
-	elapsed := time.Since(start)
-	log.Println(elapsed)
+func job(index int) {
+	//fmt.Println(index, "begin doing something")
+	time.Sleep(time.Duration(rand.Intn(10) * int(time.Second)))
+	//	fmt.Println(index, "done")
 }
