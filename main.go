cc2d20f8010baa0aadceaf332732b3e3137f83be
Oliver Trabhardt <oliver.trabhardt@googlemail.com>
2022-06-27 19:06:58 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"math/rand"
 	"net/http"
 	"os"
+	"os/exec"
+	"runtime"
 	"strings"
+	"sync"
 	"time"
 )
 
-const MAX_CONCURRENT_JOBS = 30
+const MAX_CONCURRENT_JOBS = 15
 
 type result struct {
 	sumValue      int
@@ -19,7 +23,32 @@ type result struct {
 
 var statuscount = map[string]int{}
 
-func urlFuzzScanner(url string, directoryList []string, showStatus string, resultChan chan result) {
+var clear map[string]func() //create a map for storing clear funcs
+
+func init() {
+	clear = make(map[string]func()) //Initialize it
+	clear["linux"] = func() {
+		cmd := exec.Command("clear") //Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
+func CallClear() {
+	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
+	if ok {                          //if we defined a clear func for that platform:
+		value() //we execute it
+	} else { //unsupported platform
+		panic("Your platform is unsupported! I can't clear terminal screen :(")
+	}
+}
+
+func urlFuzzScanner(url string, directoryList []string, showStatus string) {
 	// open the text file directoryList and read the lines in it
 	file, err := os.Open(directoryList[0])
 	if err != nil {
@@ -28,7 +57,9 @@ func urlFuzzScanner(url string, directoryList []string, showStatus string, resul
 	defer file.Close()
 	// read the lines in the text file
 	scanner := bufio.NewScanner(file)
-	waitChan := make(chan struct{}, MAX_CONCURRENT_JOBS)
+	//waitChan := make(chan struct{}, MAX_CONCURRENT_JOBS)
+	wg := sync.WaitGroup{}
+
 	count := 0
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -38,17 +69,20 @@ func urlFuzzScanner(url string, directoryList []string, showStatus string, resul
 		}
 		// get the line in the text file
 
-		waitChan <- struct{}{}
+		//waitChan <- struct{}{}
+		wg.Add(1)
 		count++
-		go func(count int, url string, line string, showStatus string, resultChan chan result) {
-			testUrl(url, line, showStatus, resultChan)
+		go func(count int, url string, line string, showStatus string) {
+			testUrl(url, line, showStatus)
 			job(count)
-			<-waitChan
-		}(count, url, line, showStatus, resultChan)
+			defer wg.Done()
+		}(count, url, line, showStatus)
 	}
+	wg.Wait()
+	return
 }
 
-func testUrl(url string, line string, showStatus string, resultChan chan result) {
+func testUrl(url string, line string, showStatus string) bool {
 	// create a new http client
 	// client := &http.Client{}
 	client := &http.Client{
@@ -62,10 +96,15 @@ func testUrl(url string, line string, showStatus string, resultChan chan result)
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println(url)
 	// set the user agent
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36")
-	// make the request w
-	resp, err := client.Do(req)
+	// define the request with a timeout of 5 seconds
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	// make the request
+	resp, err := client.Do(req.WithContext(ctx))
+
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -78,32 +117,35 @@ func testUrl(url string, line string, showStatus string, resultChan chan result)
 		// if the response status code is not 200, print the url and the response status code
 		fmt.Println(url + " " + resp.Status)
 	}
-	res := result{sumValue: 1, multiplyValue: resp.StatusCode}
-	resultChan <- res
+	//res := result{sumValue: 1, multiplyValue: resp.StatusCode}
+	//CallClear()
+	//fmt.Println(statuscount)
+	//resultChan <- res
+	return true
 	// return the status code of the response
 }
 
 func main() {
 	// get url parameter from name "url" in the command line
 	url := os.Args[1]
-	resultChan := make(chan result, 1)
+	//resultChan := make(chan result, 1)
 	// get directoryList parameter from name "directoryList" in the command line
 	directoryList := strings.Split(os.Args[2], ",")
 	// get status parameter from the command lline
 	status := os.Args[3]
 
 	// check the directory list, if the found in the url
-	urlFuzzScanner(url, directoryList, status, resultChan)
+	urlFuzzScanner(url, directoryList, status)
 
-	res := <-resultChan
-	fmt.Printf("Sum Value: %d\n", res.sumValue)
-	fmt.Printf("Multiply Value: %d\n", res.multiplyValue)
+	//	res := <-resultChan
+	//fmt.Printf("Sum Value: %d\n", res.sumValue)
+	//fmt.Printf("Multiply Value: %d\n", res.multiplyValue)
 	fmt.Println(statuscount) // map[string]int
-	close(resultChan)
+	//	close(resultChan)
 }
 
 func job(index int) {
 	//fmt.Println(index, "begin doing something")
-	time.Sleep(time.Duration(rand.Intn(10) * int(time.Second)))
+	time.Sleep(time.Duration(rand.Intn(1) * int(time.Second)))
 	//	fmt.Println(index, "done")
 }
