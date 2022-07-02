7b55253cba9f6f0bf22e44be68dd11f40a6ca0a6
Oliver Trabhardt <oliver.trabhardt@googlemail.com>
2022-07-02 13:50:37 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+export GOPATH=$PWD
+
+set -ex
+rm -rf tmp
+mkdir -p tmp
+GOOS=linux GOARCH=386 go build -o tmp/webdir-linux32.bin main.go
+GOOS=windows GOARCH=386 go build -o tmp/webdir-win32.exe main.go
+GOOS=darwin GOARCH=amd64 go build -o tmp/webdir-mac64.bin main.go
+
+chmod +x tmp/*
+upx -9 tmp/*
